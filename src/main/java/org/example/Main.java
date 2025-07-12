03bae148065352c7ac323f4ab2ae1ab1ecfad46b
Youssef Fawzy <youssef@example.com>
2025-07-12 16:22:41 -0700

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package main.java.org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // Sample test case
+        // read the string from the user
+        String input = "babad";
+        String result = longestPalindrome(input);
+        System.out.println("Longest Palindromic Substring of \"" + input + "\": " + result);
+
+    }
+
+    public static String longestPalindrome(String s) {
+        // TODO: Implement the logic
+        return "";
+    }
+}
