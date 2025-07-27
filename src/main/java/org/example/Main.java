8d303d77ff3ccadc1eba8057740ebc34dbff96ea
Youssefawzy <116081592+Youssefawzy@users.noreply.github.com>
2025-07-27 07:08:35 -0700

    Update Main.java
    Base Assignment

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,16 +4,7 @@ package main.java.org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // Sample test case
-        // read the string from the user
-        String input = "babad";
-        String result = longestPalindrome(input);
-        System.out.println("Longest Palindromic Substring of \"" + input + "\": " + result);
+   
 
-    }
-
-    public static String longestPalindrome(String s) {
-        // TODO: Implement the logic
-        return "";
     }
 }
